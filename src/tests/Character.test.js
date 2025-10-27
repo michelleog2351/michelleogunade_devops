58967c4111ba28168b827454d9e48cf8630d3c52
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-10-27 00:47:31 +0000

    Docs(test): Add extra condition check to Character.test.js

--- src/tests/Character.test.js
+++ src/tests/Character.test.js
@@ -1,9 +1,16 @@
+// mocha test for Character class
+
 const assert = require("assert");
 const Character = require("../Character");
 
 describe("Character Class", function () {
   it("should return the correct name", function () {
-    const character = new Character("Michelle", 100, 50);
+    const character = new Character("Michelle");
     assert.strictEqual(character.getName(), "Michelle");
+  });
+
+  it("should not match an incorrect name", function () {
+    const character = new Character("Nezuko");
+    assert.notStrictEqual(character.getName(), "Yoshino");
   });
 });
